ff2bb1ca3f7b8855c556619cf3fa08b5a2e9f58e
Fabian Lorenz <fabianlorenz97@gmail.com>
2022-12-01 20:29:20 +0100

    Day 1

---
+++ 01/day1.py
@@ -0,0 +1,16 @@
+input = open("inputs.txt", "r").read().split("\n")
+
+sum = 0
+sum_list = []
+
+for x in input:
+    if x == "":
+        sum_list.append(sum)
+        sum = 0
+    else:
+        sum = sum + int(x)
+
+sum_list.sort(reverse=True);
+
+print(sum_list[0]) #Part 1
+print(sum_list[0]+sum_list[1]+sum_list[2]) #Part 2
